fbb7f6bb7af476a9e3a781a375ffe6016c8faa23
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-06-18 08:10:09 +0300

    Update the demo and its configuration, while documenting them a bit.

--- demo/Main.hs
+++ demo/Main.hs
@@ -1,19 +1,32 @@
 module Main where
 
-import BasePrelude
 import Control.Lens.Basic
 
 
+-- Uses the Strict Record syntax.
 type Person =
-  {! name :: String, birthday :: {! year :: Int, month :: Int, day :: Int } }
+  {! 
+    name :: String, 
+    birthday :: {! year :: Int, month :: Int, day :: Int }, 
+    country :: Country
+  }
 
+-- Uses the Lazy Record syntax.
+type Country =
+  {~
+    name :: String,
+    language :: String
+  }
+
+person :: Person
 person =
   {! 
     name = "Yuri Alekseyevich Gagarin", 
     birthday = {! year = 1934, month = 3, day = 9 },
-    country = {! name = "Soviet Union", language = "Russian" }
+    country = {~ name = "Soviet Union", language = "Russian" }
   }
 
+-- Uses the Field-label Lens syntax.
 main = do
   putStrLn $ "Name: " <> show (view @name person)
   putStrLn $ "Country: " <> show (view (@country . @name) person)
